ff0d312a77ef41a030d9511262c1ab6453d95c08
Карина Вейс <qvii@MacBook-Air-Karina.local>
2026-04-03 18:41:47 -0700

    feat: create base files and types

--- packages/my-react-flow/src/types/index.ts
+++ packages/my-react-flow/src/types/index.ts
@@ -1,13 +1,23 @@
 import type { CSSProperties } from "react";
 
+export const PositionsList = {
+  Top: "top",
+  Bottom: "bottom",
+  Left: "left",
+  Right: "right",
+} as const;
+
+export type Positions = keyof typeof PositionsList;
+
 export interface Coordinates {
   x: number;
   y: number;
 }
 
 export interface Node {
-  id: number;
+  id: string;
   coordinates: Coordinates;
+  __internal: unknown;
   data?: unknown;
   styles?: CSSProperties;
   sourcePosition?: string;
@@ -15,12 +25,23 @@ export interface Node {
 }
 
 export interface NodeProps {
-  id: number;
+  id: string;
   coordinates: Coordinates;
   data?: unknown;
   styles?: CSSProperties;
   sourcePosition?: string;
   targetPosition?: string;
+}
+
+export interface Edge {
+  id: string;
+  sourceXY: Coordinates;
+  targetXY: Coordinates;
+}
+
+export interface BezierEgde extends Edge {
+  sourcePosition: Positions;
+  targetPosition: Positions;
 }
 
 export type GridTypes = "lines" | "dots";
